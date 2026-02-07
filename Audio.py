7ab942e4fe99767b275c625c76d450266718cf07
Pladmoir <trandhawa2007@gmail.com>
2026-02-07 01:10:15 -0500

    Fixed audio loop

--- Audio.py
+++ Audio.py
@@ -1,25 +1,34 @@
 import sounddevice as sd
 import numpy as np
+import threading
+import queue
 import time
 
 #======= Iniitialize parameters ========
 
-duration = 1  # seconds
-sample_rate = 16000  # Sample rate
+duration = 0.5  # seconds
+sample_rate = 16000  # Sample 
+sample_size = int(sample_rate * duration)  # bits per sample
+
 sd.default.samplerate = sample_rate
-sd.default.channels = 2  
+sd.default.channels = 1
+
 
 #============ Audio Loop ================
+print("Recording audio. Ctrl+C to stop.")
 
-try:
-    while True:
-        print("Recording audio...")
-        myrecording = sd.rec(int(duration * sample_rate))
-        sd.wait()  # Wait until recording is finished
-        sd.play(myrecording, sample_rate)
-        sd.wait()  # Wait until playback is finished
-except KeyboardInterrupt:
-    print("Audio recording stopped.")
+def audio_callback(indata, outdata, frames, time, status):
+    if status:
+        print(status)
+    outdata[:] = indata  # Echo the input to output)       
+
+with sd.Stream(callback=audio_callback, dtype='float32'):
+    try:
+        while True:
+            time.sleep(0.1)  # Keep the main thread alive
+    except KeyboardInterrupt:
+        print("Audio recording stopped.")
+
 
 
 
