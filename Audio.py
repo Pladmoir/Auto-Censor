9c0816ebc34a17f945882917a80fa633989a951b
Pladmoir <trandhawa2007@gmail.com>
2026-02-07 01:21:13 -0500

    Added rotating audio buffer

--- Audio.py
+++ Audio.py
@@ -3,12 +3,16 @@ import numpy as np
 import threading
 import queue
 import time
+from collections import deque
 
 #======= Iniitialize parameters ========
 
-duration = 0.5  # seconds
+buffer_seconds = 0.5  
 sample_rate = 16000  # Sample 
-sample_size = int(sample_rate * duration)  # bits per sample
+sample_size = int(sample_rate * buffer_seconds)  # bits per sample
+buffer_size = int(sample_rate * buffer_seconds)  # bytes per buffer (16-bit audio)
+
+audio_buffer = deque(maxlen=buffer_size)  # Circular buffer to hold audio data
 
 sd.default.samplerate = sample_rate
 sd.default.channels = 1
@@ -20,7 +24,14 @@ print("Recording audio. Ctrl+C to stop.")
 def audio_callback(indata, outdata, frames, time, status):
     if status:
         print(status)
-    outdata[:] = indata  # Echo the input to output)       
+    audio_buffer.extend(indata[:, 0])  # Add incoming audio to the buffer   
+    if len(audio_buffer) >= buffer_size:
+        outdata[:] = np.zeros() 
+    else:
+        chunk = np.array(
+            [audio_buffer.popleft() for _ in range(frames)], dtype='float32') 
+        outdata[:, 0] = chunk
+         
 
 with sd.Stream(callback=audio_callback, dtype='float32'):
     try:
