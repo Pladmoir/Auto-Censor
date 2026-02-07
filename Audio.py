5c08ec6f84427b5c79362aa3caceabcdaf97dacd
Pladmoir <trandhawa2007@gmail.com>
2026-02-07 00:28:30 -0500

    Looping input/output audio

--- Audio.py
+++ Audio.py
@@ -2,23 +2,24 @@ import sounddevice as sd
 import numpy as np
 import time
 
-#=======Iniitialize parameters========
+#======= Iniitialize parameters ========
 
 duration = 1  # seconds
 sample_rate = 16000  # Sample rate
 sd.default.samplerate = sample_rate
 sd.default.channels = 2  
 
-#=======Record audio========
+#============ Audio Loop ================
 
-print("Recording audio...")
-myrecording = sd.rec(int(duration * sample_rate))
+try:
+    while True:
+        print("Recording audio...")
+        myrecording = sd.rec(int(duration * sample_rate))
+        sd.wait()  # Wait until recording is finished
+        sd.play(myrecording, sample_rate)
+        sd.wait()  # Wait until playback is finished
+except KeyboardInterrupt:
+    print("Audio recording stopped.")
 
-sd.wait()  # Wait until recording is finished
 
-#=======Playback audio========
 
-print("Recording finished.")
-print("Playing back the recording...")
-sd.play(myrecording, sample_rate)
-sd.wait()  # Wait until playback is finished
